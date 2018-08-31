380dd273ef19e974d2641c42394cdd60daf82e41
bipsen <36302567+bipsen@users.noreply.github.com>
2018-08-31 21:55:45 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,53 +1,62 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[2]:
 
 
 from chatterbot import ChatBot
-import nltk
-import time
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
+import nltk
 import operator
 
+
+def run_Bot(text):
+    """This is the main function to run the chatbot, analyse 
+    the responses with nltk and send OSC messages to pure data.
+    """
+    
+    #Get chatbot response from the user input.
+    bot_response = chatbot.get_response(text).text 
+    
+    #Print response in bold.
+    print('\033[1m' + bot_response + '\033[0m')
+    
+    #Get polarity score from chatbot response.
+    analysis = vader_analyzer.polarity_scores(text)
+    
+    #change polarity score relatively to a audible frequency. 
+    freq = (analysis['compound'] - -1) / (1 - -1) * (800 - 200) + 200
+    
+    #Send OSC message, to be listened to by pd.
+    client.send_message("/filter", freq)
+    
+    
+# Set up chatbot.
 chatbot = ChatBot(
     'Ron Obvious',
     trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
 )
 
-# Train based on the english corpus
+# Train based on the english corpus.
 chatbot.train("chatterbot.corpus.english")
 
-#download lexicon for nltk
+# Download lexicon for nltk.
 nltk.download('vader_lexicon')
 
-#set up a OSC client
+# Set up sentiment analyzer.
+vader_analyzer = SentimentIntensityAnalyzer()
+
+# Set up OSC client.
 ip = 'localhost'
 port = 9000
 client = udp_client.SimpleUDPClient(ip, port)
 
 
-def ana(text):
-    response = chatbot.get_response(text).text
-    print('\033[1m' + response + '\033[0m')
-    demo_vader_instance(response)
-
-
-def demo_vader_instance(text):
-    global analysis
-    from nltk.sentiment import SentimentIntensityAnalyzer
-    vader_analyzer = SentimentIntensityAnalyzer()
-    analysis = vader_analyzer.polarity_scores(text)
-    amp = (analysis['compound'] - -1) / (1 - -1) * (800 - 200) + 200
-
-    client.send_message("/filter", amp)
-
-
 while True:
-    a_str = input('Talk (empty to exit): ')
-    if not a_str:   # Exit on empty string.
+    user_response = input('Talk (empty to exit): ')
+    if not user_response:   # Exit on empty string.
         break
     else:
-        ana(a_str)
+        run_Bot(user_response)
 
